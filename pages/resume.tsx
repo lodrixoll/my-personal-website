149dcf02168d22a6b0589771f3731c70083d8ea7
lodrixoll <lodrixoll@gmail.com>
2024-05-01 17:13:03 -0700

    add resume route

---
+++ pages/resume.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Resume: React.FC = () => {
+    return (
+        <div className="container">
+            <div className="row justify-content-center my-5">
+                <div className="col-12">
+                    <h1 className="text-center text-white">Resume</h1>
+                    {/* Resume content goes here */}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Resume;
