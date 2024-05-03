df8c413986cbbb6d01b615f9200e5b4e563703b5
lodrixoll <lodrixoll@gmail.com>
2024-05-03 16:27:31 -0700

    fix button margin for mobile responsive design

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,25 +6,25 @@ const Navbar = () => {
     <div className="row justify-content-center my-5">
                 <div className="col-auto">
                     <Link href="/">
-                        <button className="btn btn-outline-primary btn-lg mx-2 text-white">Home</button>
+                        <button className="btn btn-outline-primary btn-lg mx-2 my-1 text-white">Home</button>
                     </Link>
                     {/* <Link href="#">
                         <button className="btn btn-outline-primary mx-2 text-white">About</button>
                     </Link> */}
                     <Link href="#">
-                        <button className="btn btn-outline-primary btn-lg mx-2 text-white">Projects</button>
+                        <button className="btn btn-outline-primary btn-lg mx-2 my-1 text-white">Projects</button>
                     </Link>
                     <Link href="https://github.com/lodrixoll" target="_blank" rel="noopener noreferrer">
-                        <button className="btn btn-outline-primary btn-lg mx-2 text-white">GitHub</button>
+                        <button className="btn btn-outline-primary btn-lg mx-2 my-1 text-white">GitHub</button>
                     </Link>
                     <Link href="https://www.linkedin.com/in/liam-o-driscoll/" target="_blank" rel="noopener noreferrer">
-                        <button className="btn btn-outline-primary btn-lg mx-2 text-white">LinkedIn</button>
+                        <button className="btn btn-outline-primary btn-lg mx-2 my-1 text-white">LinkedIn</button>
                     </Link>
                     <Link href="mailto:lodrixoll@gmail.com" target="_blank" rel="noopener noreferrer">
-                        <button className="btn btn-outline-primary btn-lg mx-2 text-white">Email</button>
+                        <button className="btn btn-outline-primary btn-lg mx-2 my-1 text-white">Email</button>
                     </Link>
                     <Link href="/resume" rel="noopener noreferrer">
-                        <button className="btn btn-outline-primary btn-lg mx-2 text-white">Resume</button>
+                        <button className="btn btn-outline-primary btn-lg mx-2 my-1 text-white">Resume</button>
                     </Link>
                 </div>
             </div>
