1fb9962029028bbe5febce1af3e5599ad35e36d1
lodrixoll <lodrixoll@gmail.com>
2024-05-02 21:25:36 -0700

    bug fixes for deployment

--- components/Info.tsx
+++ components/Info.tsx
@@ -7,7 +7,7 @@ const Info = () => {
             <img src="/images/headshot.png" alt="Headshot" className="img-fluid" style={{ borderRadius: '50%', width: '200px', height: '200px' }} />
         </div>
         <div className="col-auto">
-            <h1 className="text-white">👋 Hey, I'm Liam <span className="fs-6" style={{ color: '#007BFF', fontSize: '1.5rem' }}>@lodrixoll</span></h1>
+            <h1 className="text-white">👋 Hey, I&apos;m Liam <span className="fs-6" style={{ color: '#007BFF', fontSize: '1.5rem' }}>@lodrixoll</span></h1>
             <p className="text-white pt-2">AI Engineer, Full Stack Developer, and Entrepreneur.</p>
             <p className="text-white">Currently working on...</p>
             {/* <a href="https://github.com/lodrixoll" target="_blank" rel="noopener noreferrer">
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,30 +1,31 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Navbar = () => {
   return (
     <div className="row justify-content-center my-5">
                 <div className="col-auto">
-                    <a href="/">
+                    <Link href="/">
                         <button className="btn btn-outline-primary btn-lg mx-2 text-white">Home</button>
-                    </a>
-                    {/* <a href="#">
+                    </Link>
+                    {/* <Link href="#">
                         <button className="btn btn-outline-primary mx-2 text-white">About</button>
-                    </a> */}
-                    <a href="#">
+                    </Link> */}
+                    <Link href="#">
                         <button className="btn btn-outline-primary btn-lg mx-2 text-white">Projects</button>
-                    </a>
-                    <a href="https://github.com/lodrixoll" target="_blank" rel="noopener noreferrer">
+                    </Link>
+                    <Link href="https://github.com/lodrixoll" target="_blank" rel="noopener noreferrer">
                         <button className="btn btn-outline-primary btn-lg mx-2 text-white">GitHub</button>
-                    </a>
-                    <a href="https://www.linkedin.com/in/liam-o-driscoll/" target="_blank" rel="noopener noreferrer">
+                    </Link>
+                    <Link href="https://www.linkedin.com/in/liam-o-driscoll/" target="_blank" rel="noopener noreferrer">
                         <button className="btn btn-outline-primary btn-lg mx-2 text-white">LinkedIn</button>
-                    </a>
-                    <a href="mailto:lodrixoll@gmail.com" target="_blank" rel="noopener noreferrer">
+                    </Link>
+                    <Link href="mailto:lodrixoll@gmail.com" target="_blank" rel="noopener noreferrer">
                         <button className="btn btn-outline-primary btn-lg mx-2 text-white">Email</button>
-                    </a>
-                    <a href="/resume" rel="noopener noreferrer">
+                    </Link>
+                    <Link href="/resume" rel="noopener noreferrer">
                         <button className="btn btn-outline-primary btn-lg mx-2 text-white">Resume</button>
-                    </a>
+                    </Link>
                 </div>
             </div>
   );
