f25a9d098d32fad837119f2de70843f7d767083c
lodrixoll <lodrixoll@gmail.com>
2024-05-03 16:24:00 -0700

    add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import '../public/styles/global.css';
 import '../public/styles/bootstrap.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
+          <Head>
+              <link rel="icon" href="/images/icons/favicon.png" />
+              <title>Liam O'Driscoll | Portfolio</title>
+          </Head>
           <Component {...pageProps} />
         </>
     );
