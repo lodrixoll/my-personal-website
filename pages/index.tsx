03e51e0ebb275984f531dc6f886adcc675dfbd6d
lodrixoll <lodrixoll@gmail.com>
2024-05-02 21:07:15 -0700

    add clamp

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import Info from '../components/Info';
 import Activities from '../components/Activities';
+import '../public/styles/global.css';
 
 const Home: React.FC = () => {
     return (
-        <div className="container">
+        <div className="container clamp">
             <Navbar />
             <Info />
             <Activities />
