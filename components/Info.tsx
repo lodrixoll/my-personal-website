0ec3674ef904741519811f4f9026a39acffaef10
lodrixoll <lodrixoll@gmail.com>
2024-05-03 16:24:08 -0700

    remove old design

--- components/Info.tsx
+++ components/Info.tsx
@@ -10,15 +10,6 @@ const Info = () => {
             <h1 className="text-white">👋 Hey, I&apos;m Liam <span className="fs-6" style={{ color: '#007BFF', fontSize: '1.5rem' }}>@lodrixoll</span></h1>
             <p className="text-white pt-2">AI Engineer, Full Stack Developer, and Entrepreneur.</p>
             <p className="text-white">Currently working on...</p>
-            {/* <a href="https://github.com/lodrixoll" target="_blank" rel="noopener noreferrer">
-                <img src="/images/icons/github.png" alt="GitHub" className="img-fluid" style={{ width: '40px', height: '40px' }} />
-            </a>
-            <a href="https://www.linkedin.com/in/liam-o-driscoll/" target="_blank" rel="noopener noreferrer"  className="mx-2">
-                <img src="/images/icons/linkedin.webp" alt="LinkedIn" className="img-fluid" style={{ width: '40px', height: '40px' }} />
-            </a>
-            <a href="mailto:lodrixoll@gmail.com" target="_blank" rel="noopener noreferrer">
-                <img src="/images/icons/mail.png" alt="Email" className="img-fluid" style={{ width: '40px', height: '40px' }} />
-            </a> */}
         </div>
     </div>
   );
