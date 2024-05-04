966ced4fd3bccbbf34fb9be2bec85802c59fab40
lodrixoll <lodrixoll@gmail.com>
2024-05-03 18:15:19 -0700

    add navbar and layout fixes

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,19 +1,29 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
 
 const Projects = () => {
     return (
-        <div className="container mt-5">
-            <div className="row justify-content-center">
-              <h1 className="text-white">Projects</h1>
-              <div className="col-auto">
-                <img src="/images/icons/web-development.png" alt="web development" className="img-fluid" style={{ width: '100px', height: '100px' }}/>
-              </div>
-            </div>
+        <>
+            <Navbar />
+            <div className="container mt-5">
+                <div className="row justify-content-center align-items-center">
+                    <div className="col-auto">
+                        <img src="/images/icons/web-development.png" alt="web development" className="img-fluid" style={{ width: '100px', height: '100px' }}/>
+                    </div>
+                    <h1 className="text-white">Projects</h1>
+                </div>
 
-            <div className="rowmt-3">
-                <p className="text-white">Coming Soon...</p>
+                <div className="row mt-5">
+                    <div className="col-12">
+                        <div className="card bg-secondary" style={{ minHeight: '500px' }}>
+                            <div className="card-body">
+                                <p className="card-text text-white">Coming Soon...</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
