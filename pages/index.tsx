08fef34ffb45659ec69a0567b0a62401797fc377
lodrixoll <lodrixoll@gmail.com>
2024-05-13 22:31:26 -0700

    add gh graph

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import Info from '../components/Info';
 import Activities from '../components/Activities';
+import GitHubGraph from '../components/GitHubGraph';
 
 const Home: React.FC = () => {
+
     return (
         <div className="container clamp">
             <Navbar />
             <Info />
+            <GitHubGraph />
             <Activities />
         </div>
     );
